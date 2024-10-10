feaaa130e4f67d7dd7441b3dc9730c42cd9a4a77
Stefan Bloom <stefanjbloom88@gmail.com>
2024-10-10 15:18:15 -0600

    Refactor of movies controller and sad path testing. closing branch/endpoint

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,23 +3,19 @@ class Api::V1::MoviesController < ApplicationController
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
       faraday.params[:api_key] = Rails.application.credentials.dig(:tmdb, :key)
     end
-    
-    if params[:query].blank?
-      return render json: { message: "Please enter a search query", status: 422 }, status: :unprocessable_entity
-    end
 
-    if params[:query].present?
+    if params[:query].blank?
+      response = conn.get("/3/movie/top_rated")
+    else params[:query].present?
       response = conn.get("/3/search/movie") do |req|
         req.params[:query] = params[:query]
       end
-    else
-      response = conn.get("/3/movie/top_rated")
     end
 
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    massaged_response = JSON.parse(response.body, symbolize_names: true)
 
-    if parsed_response[:results].present?
-      movies = parsed_response[:results].first(20)
+    if massaged_response[:results].present?
+      movies = massaged_response[:results].first(20)
     else
       movies = []
     end
--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 RSpec.describe "Movies API", type: :request do
   describe "happy paths" do
     it "#index can retrieve 20 top rated movies" do
+      api_key = Rails.application.credentials.dig(:tmdb, :key)
       json_response = File.read('spec/fixtures/tmdb_movie_query.json')
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=8a3d711a9dffaae8269c99fbbbc192df").
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{api_key}").
         with(
           headers: {
         'Accept'=>'*/*',
@@ -30,15 +31,16 @@ RSpec.describe "Movies API", type: :request do
     end
 
     it '#index can retrieve movies based on search query param' do
+      api_key = Rails.application.credentials.dig(:tmdb, :key)
       json_response = File.read('spec/fixtures/tmdb_search_query.json')
-      stub_request(:get, "https://api.themoviedb.org/3/search/movie?query=The%20Lord%20of%20the%20Rings&api_key=8a3d711a9dffaae8269c99fbbbc192df").
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?query=The%20Lord%20of%20the%20Rings&api_key=#{api_key}").
         with(
           headers: {
         'Accept'=>'*/*',
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'User-Agent'=>'Faraday v2.10.1'
           }).
-        to_return(status: 200, body: json_response, headers: {}
+        to_return(status: 200, body: json_response, headers: {})
         
         get "/api/v1/movies?query=The%20Lord%20of%20the%20Rings"
 
@@ -50,5 +52,36 @@ RSpec.describe "Movies API", type: :request do
         movies.each do |movie|
           expect(movies[:attributes][:title]).to include("The Lord of the Rings")
         end
+      end
     end
-  end
+  # end
+
+  describe "sad paths" do
+    it '#index returns top rated movies if ?query=<nothing provided by user>' do
+      api_key = Rails.application.credentials.dig(:tmdb, :key)
+      json_response = File.read('spec/fixtures/tmdb_movie_query.json')
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{api_key}").
+        with(
+          headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v2.10.1'
+          }).
+        to_return(status: 200, body: json_response, headers: {}) 
+
+        get "/api/v1/movies?query="
+
+        expect(response).to be_successful
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data].count).to eq(20)
+
+        json[:data].each do |movie|
+          expect(movie).to have_key(:id)
+          expect(movie[:type]).to eq("movie")
+          expect(movie[:attributes]).to have_key(:title)
+          expect(movie[:attributes]).to have_key(:vote_average)
+      end
+    end
+  # end
+end
