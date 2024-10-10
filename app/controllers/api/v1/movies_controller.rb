5bc73025e02c8310d6a1595210739950ab932b58
Stefan Bloom <stefanjbloom88@gmail.com>
2024-10-09 21:01:16 -0600

    testing finally green for #index endpoint 2. refactored controller, created new fixtures/.json

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,10 +3,22 @@ class Api::V1::MoviesController < ApplicationController
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
       faraday.params[:api_key] = Rails.application.credentials.dig(:tmdb, :key)
     end
-    var = Rails.application.credentials.dig(:tmdb, :key)
-    response = conn.get("/3/movie/top_rated")
-    # binding.pry
-    movies = JSON.parse(response.body, symbolize_names: true)[:results].first(20)
+
+    if params[:query].present?
+      response = conn.get("/3/search/movie") do |req|
+        req.params[:query] = params[:query]
+      end
+    else
+      response = conn.get("/3/movie/top_rated")
+    end
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    if parsed_response[:results].present?
+      movies = parsed_response[:results].first(20)
+    else
+      movies = []
+    end
 
     render json: MovieSerializer.format_movie_list(movies)
   end
