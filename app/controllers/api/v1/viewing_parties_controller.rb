fdfe843a59533889fb9b56cdb37b6847258962fa
Stefan Bloom <stefanjbloom88@gmail.com>
2024-10-13 12:27:55 -0600

    serializer and test for viewing_parties_request initialized

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::ViewingPartiesController < ApplicationController
   before_action: authenticate_api_key
 
   def create
+    host = User.find_by(api_key: params[:api_key])
+    if host.nil?
+      render json: { error: "Host w/ API key required" }, status: unauthorized
+    end
+
     
   end
 
---
+++ spec/requests/api/v1/viewing_parties_request_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Viewing Parties Requests", type: :request do
+  describe "happy paths" do
+
+  end
+end
